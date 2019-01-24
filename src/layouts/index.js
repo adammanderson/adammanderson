0e49ae090ddfb4113d6c58527ff15e926604289d
Adam Anderson <adam.m.anderson@gmail.com>
2019-01-24 14:15:44 +0000

    Responsive meta tag

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -18,6 +18,7 @@ const Layout = ({ children }) => (
       <GlobalStyles />
       <Helmet>
         <link rel="stylesheet" href="https://use.typekit.net/nyz2kww.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       </Helmet>
       <Header />
       { children }
